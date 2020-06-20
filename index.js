8b494062f0896547514295fc8e13d228fe098688
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-20 18:41:52 +0100

    Made search function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const rp = require("request-promise");
-const cheerio = require("cheerio");
-const Table = require("cli-table");
+const fetch = require("node-fetch");
+
+const url = `https://www.imdb.com/find?&ref_=nv_sr_sm&q=`;
+
+const searchMovies = (searchTerm) => {
+  return fetch(`${url}${searchTerm}`).then((response) => response.text());
+};
+
+searchMovies("star wars").then((body) => {
+  console.log(body);
+});
