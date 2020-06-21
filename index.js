44a1678796a60db9f23e8ec3f066c1fad67c7db0
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-21 10:56:19 +0100

    Set up express

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
+const express = require("express");
 
-const url = `https://www.imdb.com/find?&ref_=nv_sr_sm&q=`;
+const scraper = require("./scraper");
 
-const searchMovies = (searchTerm) => {
-  return fetch(`${url}${searchTerm}`).then((response) => response.text());
-};
+const app = express();
 
-searchMovies("star wars").then((body) => {
-  const movies = [];
-  const $ = cheerio.load(body);
-  $(".findResult").each(function (i, element) {
-    const $element = $(element);
-    const $image = $element.find("td a img");
-    const $title = $element.find("td.result_text a");
-    const movie = {
-      image: $image.attr("src"),
-      title: $title.text(),
-    };
-    movies.push(movie);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Scraping is Fun!",
   });
-  console.log(movies);
 });
 
-// const top250 = () => {
-//   return fetch(`https://www.imdb.com/chart/top/`).then((response) =>
-//     response.text()
-//   );
-// };
+app.get("/search/:title", (req, res) => {
+  scraper.searchMovies(req.params.title).then((movies) => {
+    res.json(movies);
+  });
+});
 
-// top250().then((body) => {
-//   const $ = cheerio.load(body);
-//   $(".titleColumn").each(function (i, element) {
-//     const $element = $(element);
-//     console.log($element.text());
-//   });
-// });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
---
+++ scraper.js
@@ -0,0 +1,42 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+
+const url = `https://www.imdb.com/find?&ref_=nv_sr_sm&q=`;
+
+const searchMovies = (searchTerm) => {
+  return fetch(`${url}${searchTerm}`)
+    .then((response) => response.text())
+    .then((body) => {
+      const movies = [];
+      const $ = cheerio.load(body);
+      $(".findResult").each(function (i, element) {
+        const $element = $(element);
+        const $image = $element.find("td a img");
+        const $title = $element.find("td.result_text a");
+        const movie = {
+          image: $image.attr("src"),
+          title: $title.text(),
+        };
+        movies.push(movie);
+      });
+      return movies;
+    });
+};
+
+module.exports = {
+  searchMovies,
+};
+
+// const top250 = () => {
+//   return fetch(`https://www.imdb.com/chart/top/`).then((response) =>
+//     response.text()
+//   );
+// };
+
+// top250().then((body) => {
+//   const $ = cheerio.load(body);
+//   $(".titleColumn").each(function (i, element) {
+//     const $element = $(element);
+//     console.log($element.text());
+//   });
+// });
